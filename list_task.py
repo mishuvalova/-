d12c00ecab8360d77d986cc7601b4c84a3a68e27
shuvalova.mira <mira.shuvalova@mail.ru>
2016-09-20 20:57:28 +0300

    New linear

--- list_task.py
+++ list_task.py
@@ -1,16 +1,23 @@
 def remove_adjacent(lst):
-  l = []
-  for num in lst:
-    if len(l) == 0 or num != l[-1]:
-      l.append(num)
-      
-  return l
+  if lst:
+    l = [lst[0]]
+    for k in range(1,len(lst)):
+      if lst[k-1] != lst[k]:
+        l.append(lst[k])
+    return l  
+  return []
 
 def linear_merge(lst1, lst2):
   l = []
-  while len(lst1) and len(lst2):
-    if lst1[0] < lst2[0]:
-      l.append(lst1.pop(0))
-      
+  i1 = 0
+  i2 = 0
+  while i1 < len(lst1) and i2 < len(lst2):
+    if lst1[i1] < lst2[i2]:
+      l.append(lst1[i1])
+      i1 = i1 + 1
     else:
-      l.append(lst2.pop(0))
+      l.append(lst2[i2])
+      i2 = i2 + 1
+  l.extend(lst1[i1:])
+  l.extend(lst2[i2:])
+  return l
