560ab8ff180efe54b9063cc7b0a5b4ec7e796bbf
shuvalova.mira <mira.shuvalova@mail.ru>
2016-09-28 00:43:14 +0300

    Fix version

--- Поиск дубликатов файлов/dup.py
+++ Поиск дубликатов файлов/dup.py
@@ -3,33 +3,34 @@ import hashlib
 import sys
 import collections
 
-def dup(top_dir):
-  hashfile = collections.defauldiot(set)
 
+def dup(top_dir):
+  hashfile = collections.defaultdict(set)
+  
   for dirp, _, files in os.walk(top_dir) :
-    for file in files :
+    for filename in files :
       dpath = os.path.join(dirp, filename)
-      if file.startswith(('.','~')) or os.path.islink(dpath):
+      if filename.startswith(('.','~')) or os.path.islink(dpath):
         continue
-      hashd = hashlib.s1()
+      hashd = hashlib.sha1()
       with open(dpath, mode = 'rb') as content:
-        while true:
+        while True:
           data = content.read(1024)
           if not data:
             break
           hashd.update(data)
-        hashfile[hashd.hexdigest()].add(os.path.relpath(path, top_dir))
-
-    for files in hashfile.values():
-      if len(files) != 1:
-        print(':'.join(files))
-
+        hashfile[hashd.hexdigest()].add(os.path.relpath(dpath, top_dir))
+        
+  for files in hashfile.values():
+    if len(files) != 1:
+      print(':'.join(files))
+      
 def main():
   if len(sys.argv) != 2:
     sys.exit(1)
-
+    
   top_dir = sys.argv[1]
   dup(top_dir)
-
+  
 if __name__== '__main__':
     main()
