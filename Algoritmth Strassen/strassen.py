b2b168ec34ddc562adcb804fd64ef906b23a3976
shuvalova.mira <mira.shuvalova@mail.ru>
2016-10-06 00:17:10 +0300

    New version

--- Algoritmth Strassen/strassen.py
+++ Algoritmth Strassen/strassen.py
@@ -1,6 +1,12 @@
-import sys
 import numpy as np
-import numpy
+
+
+def read_matrix(n, m):
+    c = []
+    for _ in range(n):
+        c.append([int(x) for x in input().split()])
+    m[:n, :n] = np.matrix(c)
+    return m
 
 
 def split_matrix(m):
@@ -10,23 +16,23 @@ def split_matrix(m):
     return m11, m12, m21, m22
 
 
-def strassen(m1, m2):
-    if np.size(m1) == 1:
-        return np.dot(m1, m2)
+def strassen(a, b):
+    if np.size(a) == 1:
+        return np.dot(a, b)
     else:
-        a11, a12, a21, a22 = split_matrix(m1)
-        b11, b12, b21, b22 = split_matrix(m2)
-        s1 = strassen(a11 + a22, b11 + b22)
-        s2 = strassen(a21 + a22, b11)
-        s3 = strassen(a11, b12 - b22)
-        s4 = strassen(a22, b21 - b11)
-        s5 = strassen(a11 + a12, b22)
-        s6 = strassen(a21 - a11, b11 + b12)
-        s7 = strassen(a12 - a22, b21 + b22)
-        c11 = s1 + s4 - s5 + s7
-        c12 = s3 + s5
-        c21 = s2 + s4
-        c22 = s1 - s2 + s3 + s6
+        a11, a12, a21, a22 = split_matrix(a)
+        b11, b12, b21, b22 = split_matrix(b)
+        p1 = strassen(a11 + a22, b11 + b22)
+        p2 = strassen(a21 + a22, b11)
+        p3 = strassen(a11, b12 - b22)
+        p4 = strassen(a22, b21 - b11)
+        p5 = strassen(a11 + a12, b22)
+        p6 = strassen(a21 - a11, b11 + b12)
+        p7 = strassen(a12 - a22, b21 + b22)
+        c11 = p1 + p4 - p5 + p7
+        c12 = p3 + p5
+        c21 = p2 + p4
+        c22 = p1 - p2 + p3 + p6
         return np.vstack((np.hstack((c11, c12)),
                           np.hstack((c21, c22))))
 
@@ -38,22 +44,13 @@ def print_matrix(m):
 
 def main():
     n = int(input())
-    n_round = 1
-    while n_round < n:
-        n_round *= 2
-    m1 = np.zeros((n_round, n_round), dtype=int)
-    m2 = np.zeros((n_round, n_round), dtype=int)
-    a = []
-    for i in range(n):
-        a.append([int(x) for x in input().split()])
-    m1[:n, :n] = numpy.matrix(a)
-    b = []
-    for i in range(n):
-        b.append([int(x) for x in input().split()])
-    m2[:n, :n] = numpy.matrix(b)
-
-    resm = strassen(m1, m2)
-    print_matrix(resm[:n, :n])
+    n_widening = 1
+    while n_widening < n:
+        n_widening *= 2
+    a = np.zeros((n_widening, n_widening), dtype=int)
+    b = np.zeros((n_widening, n_widening), dtype=int)
+    result = strassen(read_matrix(n, a), read_matrix(n, b))
+    print_matrix(result[:n, :n])
 
 
 if __name__ == '__main__':
